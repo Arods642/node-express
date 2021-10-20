2c41b14d40b92ba86e9af2adaebc9df12d7bcbd2
anthony rodriguez <anthony@arod.la>
2021-10-20 13:53:06 -0700

    second

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const http = require('http');
+const _ = require('lodash');
 
 
-const server = http.createServer( (req, res) => {
-if(req.url === '/') {
-    res.end('welcome to our home page')
-}
-if(req.url === '/about') {
-    res.end('about page.')
-} 
-res.end(
-    `<h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for.</p>`
-    
-)
-    
+const items = [1, [2, [3, [4,]]]]
 
-})
-
-server.listen(5000);
+const newItems = _.flattenDeep(items);
+console.log(newItems);
